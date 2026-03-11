f11fa342e1a7f4b857b7368156e58a9b8485bd91
AnoshR <anoshrukadikar@gmail.com>
2026-03-11 22:40:24 +0530

    Day-3: modularized vpc and ec2

--- main.tf
+++ main.tf
@@ -2,3 +2,15 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr              = "10.0.0.0/16"
+  private_subnet_cidr = "10.0.2.0/24"
+  public_subnet1_cidr = "10.0.1.0/24"
+  public_subnet2_cidr = "10.0.4.0/24"
+  
+  az1                   = "eu-north-1a"
+  az2                   = "eu-north-1b"
+  az3                   = "eu-north-1c"
+}
